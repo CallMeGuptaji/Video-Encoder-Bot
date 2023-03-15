ac0e46ac68bd54debce47197898ad04720cd1a3e
Pratyush Gupta <55276498+CallMeGuptaji@users.noreply.github.com>
2023-03-15 17:13:49 +0530

    Update __main__.py

--- VideoEncoder/__main__.py
+++ VideoEncoder/__main__.py
@@ -39,62 +39,25 @@ async def main():
     print(f'[Started]: @{(await app.get_me()).username}')
     await idle()
     await app.stop()
-    def help_command(update, context):
-
-    """Send a message when the command /help is issued."""
-
-    update.message.reply_text('This bot can compress/encode videos in h264 format. To use it, simply send a video file and use the /encode command to compress it.')
-
-    
-
-COMMAND_HANDLERS = {
-
-    '/start': start_command,
-
-    '/help': help_command,
-
-    '/encode': encode_command,
-
-    '/mediainfo': mediainfo_command,
-
-}
 
 def start_command(update, context):
     """Send a message when the command /start is issued."""
     update.message.reply_text('Hello, welcome to the Video Encoder Bot!')
 
+
 def help_command(update, context):
     """Send a message when the command /help is issued."""
-    update.message.reply_text('Here are the available commands:\n\n'
-                              '/start - Start the bot\n'
-                              '/help - Show this help message\n'
-                              '/encode - Compress/Encode a video in h264 format\n'
-                              '/mediainfo - Get technical information about a video file')
+    update.message.reply_text('/encode - encode/compress a video\n'
+                              '/mediainfo - get technical information about a video')
 
 
-def mediainfo_command(bot, update):
-
-    chat_id = update.message.chat_id
-
-    # Check if a video file was sent with the command
-
-    if not update.message.video:
-
-        bot.send_message(chat_id=chat_id, text='Please send a video file with the /mediainfo command.')
-
-COMMAND_HANDLERS = {
-    '/start': start_command,
-    '/help': help_command,
-    '/encode': encode_command,
-    '/mediainfo': mediainfo_command,
-}
-
-def mediainfo_command(update, context):
+def encode_command(update, context):
+    """Compress a video using h264 codec."""
     chat_id = update.message.chat_id
 
     # Check if a video file was sent with the command
     if not update.message.video:
-        update.message.reply_text('Please send a video file with the /mediainfo command.')
+        context.bot.send_message(chat_id=chat_id, text='Please send a video file with the /encode command.')
         return
 
     # Get the file ID of the video file
@@ -103,16 +66,43 @@ def mediainfo_command(update, context):
     # Download the video file
     video_file = context.bot.get_file(file_id)
 
+    # Encode the video using h264 codec and save it as a file
+    new_file_path = f'{file_id}.mp4'
+    subprocess.run(['ffmpeg', '-i', video_file.download_as_bytearray(), '-c:v', 'libx264', new_file_path])
+
+    # Send the compressed video file back to the user
+    with open(new_file_path, 'rb') as f:
+        context.bot.send_video(chat_id=chat_id, video=f)
+
+    # Delete the compressed video file from the server
+    os.remove(new_file_path)
+
+
+def mediainfo_command(bot, update):
+    chat_id = update.message.chat_id
+
+    # Check if a video file was sent with the command
+    if not update.message.video:
+        bot.send_message(chat_id=chat_id, text='Please send a video file with the /mediainfo command.')
+        return
+
+    # Get the file ID of the video file
+    file_id = update.message.video.file_id
+
+    # Download the video file
+    video_file = bot.get_file(file_id)
+
     # Run the mediainfo command on the video file
     output = subprocess.check_output(['mediainfo', '--Output=JSON', video_file.download_as_bytearray()])
 
     # Send the technical information back to the user
-    update.message.reply_text(output.decode('utf-8'))
+    bot.send_message(chat_id=chat_id, text
+
 
 
 
     
-app.loop.run_until_complete(main())
+
 
 
 

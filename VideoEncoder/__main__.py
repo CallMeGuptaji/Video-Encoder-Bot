f2edb5d8854bd8fc69d6a1fedd938f7335b155da
Pratyush Gupta <55276498+CallMeGuptaji@users.noreply.github.com>
2023-03-15 16:04:53 +0530

    Update __main__.py

--- VideoEncoder/__main__.py
+++ VideoEncoder/__main__.py
@@ -9,12 +9,24 @@
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
+# GNU Affero General Pu
+blic License for more details.
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+import logging 
+import os
+
+import subprocess
+
 import logging
+
+from telegram.ext import CommandHandler, MessageHandler, Filters, Updater
+
+# Rest of the code...
+
+
 from os import supports_dir_fd
 from pyrogram import idle
 from . import app, sudo_users
@@ -25,4 +37,44 @@ async def main():
     await idle()
     await app.stop()
 
+COMMAND_HANDLERS = {
+
+    '/start': start_command,
+
+    '/help': help_command,
+
+    '/encode': encode_command,
+
+    '/mediainfo': mediainfo_command,
+
+}
+
+def mediainfo_command(bot, update):
+
+    chat_id = update.message.chat_id
+
+    # Check if a video file was sent with the command
+
+    if not update.message.video:
+
+        bot.send_message(chat_id=chat_id, text='Please send a video file with the /mediainfo command.')
+
+        return
+
+    # Get the file ID of the video file
+
+    file_id = update.message.video.file_id
+
+    # Download the video file
+
+    video_file = bot.get_file(file_id)
+
+    # Run the mediainfo command on the video file
+
+    output = subprocess.check_output(['mediainfo', '--Output=JSON', video_file.download_as_bytearray()])
+
+    # Send the technical information back to the user
+
+    bot.send_message(chat_id=chat_id, text=output.decode('utf-8'))
+    
 app.loop.run_until_complete(main())
